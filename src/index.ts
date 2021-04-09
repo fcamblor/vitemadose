b935aab905eb662b6fc0cd5d30425e1120704883
fcamblor <fcamblor@gmail.com>
2021-04-09 14:46:38 +0200

    301, moved to https://github.com/CovidTrackerFr/vitemadose-front

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import './styles/global.scss'
-import './vmd-app.component'
-import './components/vmd-tranche-age-selector.component'
-import './components/vmd-departement-selector.component'
-import './views/vmd-home.view'
-import './views/vmd-rdv.view'
-import './views/vmd-centres.view'
-import './components/vmd-appointment-card.component'
